6e7a1b05d8653d9c1cab2b1b3a96fb0b559cface
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-14 01:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
-  // write your code here
-}
+    // write your code here
+    let password="";
+    const str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    for(let i=0 ; i<l ; i++){
+   password=password+str.charAt(Math.floor(Math.random()*str.length));
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+   
+
+    }return password;
+  }
+  
+  // Do not change the code below.
+  const l = prompt("Enter a number.");
+  alert(makeid(l));
